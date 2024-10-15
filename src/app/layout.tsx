2f726408a22f017d8e2a59970846ed2943e93057
Brandon Corn <brandon.corn@vercel.com>
2024-10-15 12:33:02 -0600

    remove export cause linter screams!

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { Header } from "@/components/Header/Header";
-import { SponsorAd } from "@/components/Ads";
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
